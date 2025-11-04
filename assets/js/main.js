15ac5e7a11fe81d756bbe9a9a5acca9e7e36d1ac
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2025-11-04 18:28:33 +0200

    added the sign up and sign in pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,21 @@ const testimonialsSwiper = new Swiper('.testimonlia.soiwper', {
     },
 });
 
+// 🟢 Eye Icon
+document.addEventListener("DOMContentLoaded", function () {
+    const togglePassword = document.querySelector("#togglePassword");
+    const passwordInput = document.querySelector("#password");
+
+    if (togglePassword && passwordInput) {
+        togglePassword.addEventListener("click", function () {
+            const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+            passwordInput.setAttribute("type", type);
+            this.classList.toggle("fa-eye");
+            this.classList.toggle("fa-eye-slash");
+        });
+    }
+});
+
+
+
 
